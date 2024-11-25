29cec8d3ee28a15f55d1d18da75d717760d2fc30
Yoseph <yosephalpha2023@gmail.com>
2024-11-24 17:22:15 -0800

    gsap initalization

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,9 +1,57 @@
-
+import gsap from "gsap";
+import { useGSAP } from '@gsap/react';
+import { heroVideo, smallHeroVideo } from '../utils';
+import { useState, useEffect } from 'react';
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth < 760 ? smallHeroVideo : heroVideo // `window` should be lowercase
+  );
+  const handleVideoSrcSet = () => {
+    if (window.innerWidth < 760) {
+      setVideoSrc(smallHeroVideo)
+    }else {
+      setVideoSrc(heroVideo)
+    }
+  }
 
-export default Hero
+  // Update video source on window resize
+  useEffect(() => {
+      window.addEventListener('resize', handleVideoSrcSet);
+    return () => {
+      window.removeEventListener('resize', handleVideoSrcSet);
+   };
+   }, []);
+
+  useGSAP(() => {
+    gsap.to('#hero', { opacity: 1, delay: 2 });
+    gsap.to("#cta", { opacity: 1, y: -50, delay: 2 });
+    
+  }, []);
+
+  return (
+    <section className="w-full nav-height bg-black relative">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p id="hero" className="hero-title">iPhone 15 Pro</p>
+        <div className="md:w-10/12 w-9/12">
+          <video
+            autoPlay
+            muted
+            playsInline
+            key={videoSrc} // Ensures the video updates when the source changes
+            className="pointer-events-none">
+            <source src={videoSrc} type="video/mp4"/>
+          </video>
+        </div>
+      </div>
+      <div
+        id = "cta"
+        className ="flex flex-col items-center opacity-0 translate-y-20" >
+            <a href ="#highLights" className="btn">Buy</a>
+            <p className="font-normal text-xl">From $199/month or $999</p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
